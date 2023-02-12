e5a1cc11d188803305b80e29bcb48f9ae15855d1
temevh <temehama@gmail.com>
2023-02-12 10:35:12 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  var apiRes = "a"
+
+
+  return (
+    <div className="App">
+      <header>
+      <h1>PokeApp</h1>  
+      </header>   
+      <body>
+        <h2>Api result {apiRes}</h2>
+
+      </body>
+    </div>
+  );
+}
+
+export default App;
