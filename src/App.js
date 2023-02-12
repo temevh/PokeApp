19595764f049b6974127fa9feb2be5921f926d8b
temevh <temehama@gmail.com>
2023-02-12 11:31:46 +0200

    ability to modify the fetch with pokeName prop

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div>
-      <Pokemon/>
+      <Pokemon pokeName={"pikachu"}/>
     </div>
   );
 }
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
+import "./Pokemon.css"
 
-const Pokemon = () => {
+const Pokemon = ({pokeName}) => {
   const [pokemon, setPokemon] = useState({});
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/ditto/")
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}/`)
       .then((response) => response.json())
       .then((data) => setPokemon(data));
   }, []);
 
   return (
-    <div>
+    <div id="infodiv">
       <p>
         Name: {pokemon.name} <br />
         Weight: {pokemon.weight} <br />
