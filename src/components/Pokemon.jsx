2a0f6b93cf59d2da2dd9197c5cb684a5588fbb5c
temevh <temehama@gmail.com>
2023-02-15 08:47:23 +0200

    added ability to display picture of pokemon

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -3,15 +3,18 @@ import "./Pokemon.css"
 
 const Pokemon = ({pokeName}) => {
   const [pokemon, setPokemon] = useState({});
+  const [pokeImg, setPokeImg] = useState({})
 
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/pikachu/`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}/`)
       .then((response) => response.json())
       .then((data) => setPokemon(data));
   }, [pokeName]);
 
   const hpStat = pokemon.stats && pokemon.stats.find((stat) => stat.stat.name === "hp");
   const attackStat = pokemon.stats && pokemon.stats.find((stat) => stat.stat.name === "attack")
+  const idString = pokemon.id && pokemon.id < 100 ? `0${pokemon.id}` : pokemon.id;
+
 
   return (
     <div id="infodiv">
@@ -31,7 +34,8 @@ const Pokemon = ({pokeName}) => {
           pokemon.abilities.map((ability) => ability.ability.name).join("\n")}
           </p>
       </div>
-
+    
+      <img src={`https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${idString}.png`}/>
     </div>
   );
 };
